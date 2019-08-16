156d1477a7a87d5d5f82fbf998f8996a4439affe
Hyunju Woo <44936075+DeveloperWoo@users.noreply.github.com>
2019-08-16 07:04:31 -0400

    Add files via upload

---
+++ assets/data/ProfileData.ts
@@ -0,0 +1,7 @@
+import { Profile } from '../../app/profile';
+
+export const PROFILEDATA: Profile = {
+    pid:"991497524",
+    name: "Hyunju Woo",
+    grad_year: 2020
+}
---
+++ assets/data/productdata.ts
@@ -0,0 +1,7 @@
+import { Product } from '../../app/product';
+
+export const PRODUCTDATA: Product[] = [
+    {id: "P001", desc:"Galaxy10 brand new phone", qty: 1},
+    {id: "P002", desc:"Black backpack", qty: 1},
+    {id: "P003", desc:"Samsung white 13 inches laptop", qty: 2}
+];
